d898824a9273b5d36b6fc4175b2304da1aa01da0
Hey ಠ  ͜  ಠ <alipio-luis@hotmail.com>
2016-10-24 11:40:05 +0100

    Normal media

--- 3Dproject/3Dproject/Terrain.cs
+++ 3Dproject/3Dproject/Terrain.cs
@@ -26,6 +26,7 @@ namespace _3Dproject
         VertexPositionNormalTexture[] vertices;
 
         Texture2D texture;
+        private Vector3[,] NormalData;
 
         public Terrain(GraphicsDevice device, ContentManager content , float _yScale)
         {            
@@ -38,7 +39,7 @@ namespace _3Dproject
             effect.Texture = texture;
             effect.LightingEnabled = true;
             effect.DirectionalLight0.DiffuseColor = new Vector3(1f, 1f, 1f);
-            effect.DirectionalLight0.Direction = new Vector3(.5f, -0.8f, 0);
+            effect.DirectionalLight0.Direction = new Vector3(.5f, -1f, 0);
             worldMatrix = Matrix.Identity;          
 
             YSCALE = _yScale;
@@ -92,11 +93,11 @@ namespace _3Dproject
                 {
                     vertices[x + z * Width] = new VertexPositionNormalTexture(new Vector3(x, (float)HeightData[x, z], z)
                         , Vector3.Zero,
-                        new Vector2((float)x / 30, (float)z / 30));
+                        new Vector2((float)x /30, (float)z / 30));
                 }                
             }
 
-            short[] index = new short[Width * 2*(Height - 1)];
+            short[] index = new short[Width * 2 * (Height - 1)];
             int count = 0;
             for (int y = 0; y < Height - 1; y++)
             {
@@ -113,22 +114,63 @@ namespace _3Dproject
                 }
             }
 
-            for (int i = 0; i < index.Length / 2; i++)
+            NormalData = new Vector3[Width, Height];
+            for(int x=1;x<Width-1;x++)
             {
-                int id1 = index[2 * i];
-                int id2 = index[2 * i + 1];
-                int id3;
-                if ((2 * i + 2) >= index.Length) id3 = index[0];
-                else id3 = index[2 * i + 2];
+                for(int y=1;y<Height-1;y++)
+                {
+                    int id5 = x + 0 + (y + 0) * Width;
 
-                Vector3 upLenght = vertices[id1].Position - vertices[id3].Position;
-                Vector3 downLength = vertices[id1].Position - vertices[id2].Position;
-                Vector3 normal = Vector3.Cross(upLenght, downLength);
-                normal.Normalize();
+                    int id1 = x - 1 + (y - 1) * Width;
+                    int id2 = x + 0 + (y - 1) * Width;
+                    int id3 = x + 1 + (y - 1) * Width;
+                    int id4 = x - 1 + (y + 0) * Width;
+                    int id6 = x + 1 + (y + 0) * Width;
+                    int id7 = x - 1 + (y + 1) * Width;
+                    int id8 = x + 0 + (y + 1) * Width;
+                    int id9 = x + 1 + (y + 1) * Width;
 
-                vertices[id1].Normal = normal;
-                vertices[id2].Normal = normal;
+                    Vector3 Length1 = vertices[id5].Position - vertices[id1].Position;
+                    Vector3 Length2 = vertices[id5].Position - vertices[id2].Position;
+                    Vector3 Length3 = vertices[id5].Position - vertices[id3].Position;
+                    Vector3 Length4 = vertices[id5].Position - vertices[id4].Position;
+                    Vector3 Length5 = vertices[id5].Position - vertices[id6].Position;
+                    Vector3 Length6 = vertices[id5].Position - vertices[id7].Position;
+                    Vector3 Length7 = vertices[id5].Position - vertices[id8].Position;
+                    Vector3 Length8 = vertices[id5].Position - vertices[id9].Position;
+
+                    Vector3 normal1 = Vector3.Cross(Length2, Length1);
+                    Vector3 normal2 = Vector3.Cross(Length3, Length2);
+                    Vector3 normal3 = Vector3.Cross(Length5, Length4);
+                    Vector3 normal4 = Vector3.Cross(Length8, Length5);
+                    Vector3 normal5 = Vector3.Cross(Length7, Length8);
+                    Vector3 normal6 = Vector3.Cross(Length6, Length7);
+                    Vector3 normal7 = Vector3.Cross(Length4, Length6);
+                    Vector3 normal8 = Vector3.Cross(Length1, Length4);
+
+                    Vector3 media = (normal1 + normal2 + normal3 + normal4 + normal5 + normal6 + normal7 + normal8) / 8;
+                    media.Normalize();
+                    vertices[id5].Normal = media;
+                    NormalData[x, y] = media;
+                }
             }
+
+            //for (int i = 0; i < index.Length / 2; i++)
+            //{
+            //    int id1 = index[2 * i];
+            //    int id2 = index[2 * i + 1];
+            //    int id3;
+            //    if ((2 * i + 2) >= index.Length) id3 = index[0];
+            //    else id3 = index[2 * i + 2];
+
+            //    Vector3 upLength = vertices[id1].Position - vertices[id3].Position;
+            //    Vector3 downLength = vertices[id1].Position - vertices[id2].Position;
+            //    Vector3 normal = Vector3.Cross(upLength, downLength);
+            //    normal.Normalize();
+
+            //    vertices[id1].Normal = normal;
+            //    vertices[id2].Normal = normal;
+            //}
 
             vertexBuffer = new VertexBuffer(device, typeof(VertexPositionNormalTexture), vertices.Length, BufferUsage.None);
             vertexBuffer.SetData<VertexPositionNormalTexture>(vertices);
