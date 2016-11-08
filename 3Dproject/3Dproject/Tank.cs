69e1882f7ee5671d8f53920d80a464b1a74f4b1c
Diogo Silva <mrdrdjblend@gmail.com>
2016-11-08 18:06:34 +0000

    THEM WHEELS WORKING

--- 3Dproject/3Dproject/Tank.cs
+++ 3Dproject/3Dproject/Tank.cs
@@ -24,24 +24,37 @@ namespace _3Dproject
 
         ModelBone turretBone;
         ModelBone cannonBone;
+        ModelBone[] wheelsBones = new ModelBone[4];
+        ModelBone[] steerBones = new ModelBone[2];
+        ModelBone hatchBone;
 
         Matrix cannonTransform;
         Matrix turretTransform;
+        Matrix[] wheelsTransform = new Matrix[4];
+        Matrix[] steerTransform = new Matrix[2];
+        Matrix hatchtransform;
 
         Matrix[] boneTransforms;
 
-        float scale;
-        float turretYaw = 0f;
-        float canonPitch = 0f;
-        public float TankYaw = 0f, TankPitch = 0f;
+        float scale, steerMult = 1;
+        private float turretYaw = 0f;
+        private float canonPitch = 0f;
+
+        string[] wheelNames = { "l_front_wheel_geo", "r_front_wheel_geo", "l_back_wheel_geo", "r_back_wheel_geo" };
+        string[] steerNames = { "r_steer_geo", "l_steer_geo" };
+
+        private float TankYaw = 0f, TankPitch = 0f, wheelsRotation = 0f, steerYaw = 0, hatchRotation = 0;
 
         public Vector3 position = new Vector3(10, 0, 10);
 
-        public Tank(GraphicsDevice device,ContentManager content)
+        public Tank(GraphicsDevice device, ContentManager content)
         {
             scale = 0.01f;
 
+            //TEST
             basicEffect = new BasicEffect(device);
+            //TEST
+
             effect = new BasicEffect(device);
 
             tankModel = content.Load<Model>("Tank/tank");
@@ -53,9 +66,21 @@ namespace _3Dproject
 
             turretBone = tankModel.Bones["turret_geo"];
             cannonBone = tankModel.Bones["canon_geo"];
+            hatchBone = tankModel.Bones["hatch_geo"];
+
+            for (int i = 0; i < wheelsBones.Length; i++)
+                wheelsBones[i] = tankModel.Bones[wheelNames[i]];
+            for (int i = 0; i < steerBones.Length; i++)
+                steerBones[i] = tankModel.Bones[steerNames[i]];
 
             turretTransform = turretBone.Transform;
             cannonTransform = cannonBone.Transform;
+            hatchtransform = hatchBone.Transform;
+
+            for (int i = 0; i < wheelsBones.Length; i++)
+                wheelsTransform[i] = wheelsBones[i].Transform;
+            for (int i = 0; i < steerBones.Length; i++)
+                steerTransform[i] = steerBones[i].Transform;
 
             boneTransforms = new Matrix[tankModel.Bones.Count];
             worldMatrix = Matrix.Identity;
@@ -65,34 +90,56 @@ namespace _3Dproject
         {
             KeyboardState keyboardState = Keyboard.GetState();
 
-            if (keyboardState.IsKeyDown(Keys.Up))            
-                canonPitch += 2f;            
-            if (keyboardState.IsKeyDown(Keys.Down))            
-                canonPitch -= 2f;            
-            if (keyboardState.IsKeyDown(Keys.Right))           
-                turretYaw -= 2f;            
-            if (keyboardState.IsKeyDown(Keys.Left))            
+            if (keyboardState.IsKeyDown(Keys.Up))
+                canonPitch += 2f;
+            if (keyboardState.IsKeyDown(Keys.Down))
+                canonPitch -= 2f;
+            if (keyboardState.IsKeyDown(Keys.Right))
+                turretYaw -= 2f;
+            if (keyboardState.IsKeyDown(Keys.Left))
                 turretYaw += 2f;
 
             if (keyboardState.IsKeyDown(Keys.A))
             {
                 TankYaw += 2f;
+                steerYaw += 2f * steerMult;
             }
             if (keyboardState.IsKeyDown(Keys.D))
             {
                 TankYaw -= 2f;
+                steerYaw -= 2f * steerMult;
             }
+            if (!keyboardState.IsKeyDown(Keys.D) && !keyboardState.IsKeyDown(Keys.A))
+            {
+                if (steerYaw > 0)
+                    steerYaw -= 4f;
+                else
+                    steerYaw += 4f;
+            }
+
+            steerYaw = MathHelper.Clamp(steerYaw, -25, 25);
 
             if (keyboardState.IsKeyDown(Keys.W))
             {
                 position.X += (float)Math.Sin(MathHelper.ToRadians(TankYaw)) * 0.5f;
                 position.Z += (float)Math.Cos(MathHelper.ToRadians(TankYaw)) * 0.5f;
+                wheelsRotation += 10f;
+                steerMult = 1f;
             }
             if (keyboardState.IsKeyDown(Keys.S))
             {
                 position.X -= (float)Math.Sin(MathHelper.ToRadians(TankYaw)) * 0.2f;
                 position.Z -= (float)Math.Cos(MathHelper.ToRadians(TankYaw)) * 0.2f;
+                wheelsRotation -= 5f;
+                steerMult = -1f;
             }
+
+            if (keyboardState.IsKeyDown(Keys.O))
+                hatchRotation += 2f;
+            else if (keyboardState.IsKeyDown(Keys.I))
+                hatchRotation -= 2f;
+
+            hatchRotation = MathHelper.Clamp(hatchRotation, 0, 90);
 
             canonPitch = MathHelper.Clamp(canonPitch, -20, 90);
 
@@ -100,14 +147,14 @@ namespace _3Dproject
             position.Z = MathHelper.Clamp(position.Z, 0, (Game1.terrain.Height - 2));
 
             float minHeight = Game1.terrain.retCameraHeight(position);
-            Vector3 tankNormal = Game1.terrain.retTerrainNormal(position);                    
-           
-            position.Y = minHeight;       
+            Vector3 tankNormal = Game1.terrain.retTerrainNormal(position);
+
+            position.Y = minHeight;
         }
 
         public void Draw(GraphicsDevice device)
         {
-            Vector3 direction = Vector3.Transform(new Vector3(1,0,0), Matrix.CreateRotationY(MathHelper.ToRadians(270 + TankYaw)));
+            Vector3 direction = Vector3.Transform(new Vector3(1, 0, 0), Matrix.CreateRotationY(MathHelper.ToRadians(270 + TankYaw)));
             //direction.Normalize();
 
             Vector3 tankNormal = Game1.terrain.retTerrainNormal(position);
@@ -120,6 +167,16 @@ namespace _3Dproject
 
             turretBone.Transform = Matrix.CreateRotationY(MathHelper.ToRadians(turretYaw - TankYaw)) * turretTransform;
             cannonBone.Transform = Matrix.CreateRotationX(MathHelper.ToRadians(-canonPitch)) * cannonTransform;
+            hatchBone.Transform = Matrix.CreateRotationX(MathHelper.ToRadians(hatchRotation)) * hatchtransform;
+
+            for (int i = 0; i < wheelsBones.Length; i++)
+            {
+                wheelsBones[i].Transform = Matrix.CreateRotationX(MathHelper.ToRadians(wheelsRotation)) * wheelsTransform[i];
+            }
+            for (int i = 0; i < steerBones.Length; i++)
+            {
+                steerBones[i].Transform = Matrix.CreateRotationY(MathHelper.ToRadians(steerYaw)) * steerTransform[i];
+            }
 
             tankModel.CopyAbsoluteBoneTransformsTo(boneTransforms);
 
@@ -141,7 +198,7 @@ namespace _3Dproject
                 DrawVectors(device, position, position + tankNormal, Color.Red);
                 DrawVectors(device, position, position + tankRight, Color.Green);
                 DrawVectors(device, position, position + tankFront, Color.Black);
-                DrawVectors(device, position, position + direction, Color.HotPink);                
+                DrawVectors(device, position, position + direction, Color.HotPink);
                 //TEST
             }
         }
@@ -155,7 +212,7 @@ namespace _3Dproject
             basicEffect.CurrentTechnique.Passes[0].Apply();
             startPoint.Y += 4;
             endPoint.Y += 4;
-            var vertices = new[] { new VertexPositionColor(startPoint, color), new VertexPositionColor(endPoint, color) };
+            VertexPositionColor[] vertices = new[] { new VertexPositionColor(startPoint, color), new VertexPositionColor(endPoint, color) };
             device.DrawUserPrimitives(PrimitiveType.LineList, vertices, 0, 1);
         }
     }
